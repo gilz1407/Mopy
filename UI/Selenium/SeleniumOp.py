f08f726fd929ea944306030f531cb65a3415049d
giltz1 <giltz@rnd-hub.com>
2018-11-13 11:49:08 +0200

    fixes

--- UI/Selenium/SeleniumOp.py
+++ UI/Selenium/SeleniumOp.py
@@ -5,11 +5,16 @@ from selenium import webdriver
 
 class SeleniumOp():
     driver=None
+
     def __init__(self,pageName='UiTesting',force=False):
         if SeleniumOp.driver is None or force==True:
+            driverConfig=Cm().config["ChromeDriver"]
             options = webdriver.ChromeOptions()
-            options.add_argument("--user-data-dir=C:\\Users\\GILTZ\\AppData\\Local\\Google\\Chrome\\User Data")
-            self.driver = webdriver.Chrome(options=options)
+            options.add_argument("--user-data-dir="+driverConfig["userDataDir"])
+            if driverConfig["path"] == "":
+                self.driver = webdriver.Chrome(options=options)
+            else:
+                self.driver = webdriver.Chrome(driverConfig["path"],options=options)
             SeleniumOp.driver = self.driver
             self.driver.maximize_window()
         self.config = Cm("Xpathmapping.ini").config[pageName]
